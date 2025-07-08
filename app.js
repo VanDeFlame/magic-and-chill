a6d014743e718ccfbae3ad27b60de558945d3295
Alexis Jauge <vandeflame@gmail.com>
2025-07-08 20:35:30 -0300

    build: :hammer: Remove unnecesary live reload dependency

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const livereload = require('livereload');
-const connectLivereload = require('connect-livereload');
 
 const app = express();
 const PORT = 3000;
@@ -10,18 +9,8 @@ const PORT = 3000;
 const liveReloadServer = livereload.createServer();
 liveReloadServer.watch(path.join(__dirname, 'public'));
 
-// Inyecta el script de livereload en las páginas HTML
-app.use(connectLivereload());
-
 // Archivos estáticos
 app.use('/', express.static(path.join(__dirname, 'public')));
-
-// Recargar navegador al detectar cambios
-liveReloadServer.server.once('connection', () => {
-	setTimeout(() => {
-		liveReloadServer.refresh('/');
-	}, 100);
-});
 
 // Inicia el servidor
 app.listen(PORT, () => {
