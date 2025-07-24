c74e417433dde3f20352cd61d8a3ea7086657e89
Alexis Jauge <vandeflame@gmail.com>
2025-07-24 15:06:14 -0300

    refactor: :recycle: Add Hud class to delegate HUD layout and rendering

--- public/scripts/core/canvas/Canvas.js
+++ public/scripts/core/canvas/Canvas.js
@@ -1,21 +1,10 @@
 import { MAP_SIZE } from '../Constants.js';
 import { Camera } from './Camera.js';
+import { Hud } from './Hud.js';
 import {
 	getPxFromX,
 	getPxFromY,
 } from '../../utils/convertCoordsToPixels.function.js';
-import { calculateArrayTextWidth } from '../../utils/calculateTextWidth.function.js';
-
-const PositionHorizontalEnum = {
-	LEFT: 'left',
-	CENTER: 'center',
-	RIGHT: 'right',
-};
-const PositionVerticalEnum = {
-	TOP: 'top',
-	MIDDLE: 'middle',
-	BOTTOM: 'bottom',
-};
 
 export class Canvas {
 	constructor(gameCanvasHtml) {
@@ -30,6 +19,7 @@ export class Canvas {
 			getPxFromX(MAP_SIZE.width + 1),
 			getPxFromY(MAP_SIZE.height + 1)
 		);
+		this.hud = new Hud(this.width, this.height, this.ctx);
 	}
 
 	drawBackground() {
@@ -89,143 +79,12 @@ export class Canvas {
 	}
 
 	drawInScreen(info) {
-		const sectorMap = this.splitHudInfoInSectors(info);
-		const sectorMapWithoutLineOverrides =
-			this.avoidOverridesInLinesOfSector(sectorMap);
+		if (!info || info.length === 0) return;
 
-		for (const [key, sector] of sectorMapWithoutLineOverrides.entries()) {
-			this.drawTextInSector(sector);
-		}
-	}
+		const drawInfo = this.hud.generateHudDrawInfo(info);
 
-	splitHudInfoInSectors(infoToDraw) {
-		const padding = 10;
-		const bothSidesPadding = padding * 2;
-
-		const sectorMap = new Map();
-		infoToDraw.forEach((info) => {
-			if (info.texts.length === 0) return;
-
-			const key = `${info.positionHorizontal}-${info.positionVertical}`;
-
-			if (!sectorMap.has(key)) {
-				sectorMap.set(key, { items: [], height: 0, width: 0 });
-			}
-
-			const sector = sectorMap.get(key);
-
-			const lineHeight = info.fontSize * 1.5;
-			const estimatedTextWidth = calculateArrayTextWidth(
-				info.texts,
-				info.fontSize,
-				this.ctx
-			);
-			const infoWidth = (info.width ?? estimatedTextWidth) + bothSidesPadding;
-			const infoHeight = info.texts.length * lineHeight + bothSidesPadding;
-
-			const infoX = this.getHorizontalPositionInScreen(
-				info.positionHorizontal,
-				infoWidth
-			);
-
-			sector.items.push({
-				...info,
-				height: infoHeight,
-				width: infoWidth,
-				lineHeight,
-				padding,
-				x: infoX,
-			});
-
-			sector.height += infoHeight;
-			sector.width = Math.max(sector.width, infoWidth);
+		drawInfo.forEach((item) => {
+			this.drawByAction(item);
 		});
-		return sectorMap;
-	}
-
-	avoidOverridesInLinesOfSector(sectorMap) {
-		for (const [key, sector] of sectorMap.entries()) {
-			const [column, row] = key.split('-');
-
-			const rowYStart = this.getVerticalPositionInScreen(row, sector.height);
-			let currentRowYStart = rowYStart;
-
-			sector.items = sector.items.map((info) => {
-				const y = currentRowYStart;
-				currentRowYStart = y + info.height;
-
-				return {
-					...info,
-					y,
-				};
-			});
-		}
-		return sectorMap;
-	}
-
-	drawTextInSector(sector) {
-		sector.items.forEach((item) => {
-			const {
-				x,
-				y,
-				width,
-				height,
-				texts,
-				fontSize,
-				color,
-				padding,
-				lineHeight,
-				background,
-			} = item;
-
-			if (background) {
-				this.drawByAction({
-					action: 'fillRect',
-					x: x,
-					y: y,
-					width,
-					height,
-					fillStyle: background,
-				});
-			}
-
-			texts.forEach((text, index) =>
-				this.drawByAction({
-					action: 'fillText',
-					text,
-					x: x + padding,
-					y: y + padding + index * lineHeight,
-					fillStyle: color || 'black',
-					font: `${fontSize}px Arial`,
-					textAlign: 'start',
-					textBaseline: 'top',
-				})
-			);
-		});
-	}
-
-	getHorizontalPositionInScreen(position, width) {
-		switch (position) {
-			case PositionHorizontalEnum.LEFT:
-				return 10;
-			case PositionHorizontalEnum.CENTER:
-				return (this.width - width) / 2;
-			case PositionHorizontalEnum.RIGHT:
-				return this.width - width - 10;
-			default:
-				return 10;
-		}
-	}
-	getVerticalPositionInScreen(position, height) {
-		switch (position) {
-			case PositionVerticalEnum.TOP:
-				return 10;
-			case PositionVerticalEnum.MIDDLE:
-				return (this.height - height) / 2;
-			case PositionVerticalEnum.BOTTOM:
-				return this.height - height - 10;
-			default:
-				return 10;
-		}
 	}
 }
---
+++ public/scripts/core/canvas/Hud.js
@@ -0,0 +1,174 @@
+import { calculateArrayTextWidth } from '../../utils/calculateTextWidth.function.js';
+const PositionHorizontalEnum = {
+	LEFT: 'left',
+	CENTER: 'center',
+	RIGHT: 'right',
+};
+const PositionVerticalEnum = {
+	TOP: 'top',
+	MIDDLE: 'middle',
+	BOTTOM: 'bottom',
+};
+
+export class Hud {
+	constructor(width, height, ctx) {
+		this.width = width;
+		this.height = height;
+		this.ctx = ctx;
+		this.padding = 10;
+		this.sectorMap = new Map();
+	}
+
+	generateEmptySectors() {
+		for (const horizontal of Object.values(PositionHorizontalEnum)) {
+			for (const vertical of Object.values(PositionVerticalEnum)) {
+				this.sectorMap.set(`${horizontal}-${vertical}`, {
+					items: [],
+					height: 0,
+					width: 0,
+				});
+			}
+		}
+	}
+
+	generateHudDrawInfo(infoToDraw) {
+		this.generateEmptySectors();
+		this.splitBySectors(infoToDraw);
+		this.avoidOverridesInLinesOfSector();
+
+		const drawInfo = [];
+
+		this.sectorMap.forEach((sector) => {
+			drawInfo.push(
+				...sector.items.flatMap((item) => this.formatDrawInfo(item))
+			);
+		});
+
+		return drawInfo;
+	}
+
+	splitBySectors(infoToDraw) {
+		infoToDraw.forEach((info) => {
+			if (info.texts.length === 0) return;
+
+			const key = `${info.positionHorizontal}-${info.positionVertical}`;
+			const sector = this.sectorMap.get(key);
+
+			const infoWithSize = this.calculateInfoSize(info);
+
+			sector.items.push(infoWithSize);
+			sector.height += infoWithSize.height;
+			sector.width = Math.max(sector.width, infoWithSize.width);
+		});
+	}
+
+	avoidOverridesInLinesOfSector() {
+		this.sectorMap.forEach((sector, key) => {
+			if (sector.items.length === 0) return;
+			const [column, row] = key.split('-');
+
+			const rowYStart = this.getVerticalPositionInScreen(row, sector.height);
+			let currentRowYStart = rowYStart;
+
+			sector.items = sector.items.map((info) => {
+				const y = currentRowYStart;
+				currentRowYStart = y + info.height;
+
+				return {
+					...info,
+					y,
+				};
+			});
+		});
+	}
+
+	calculateInfoSize(info) {
+		const lineHeight = info.fontSize * 1.5;
+		const bothSidesPadding = this.padding * 2;
+		const estimatedTextWidth = calculateArrayTextWidth(
+			info.texts,
+			info.fontSize,
+			this.ctx
+		);
+		const infoWidth = (info.width ?? estimatedTextWidth) + bothSidesPadding;
+		const infoHeight = info.texts.length * lineHeight + bothSidesPadding;
+
+		const infoX = this.getHorizontalPositionInScreen(
+			info.positionHorizontal,
+			infoWidth
+		);
+
+		return {
+			...info,
+			height: infoHeight,
+			width: infoWidth,
+			lineHeight,
+			padding: this.padding,
+			x: infoX,
+		};
+	}
+
+	formatDrawInfo(item) {
+		const {
+			x,
+			y,
+			width,
+			height,
+			texts,
+			fontSize,
+			color,
+			padding,
+			lineHeight,
+			background,
+		} = item;
+
+		const info = texts.map((text, index) => ({
+			action: 'fillText',
+			text,
+			x: x + padding,
+			y: y + padding + index * lineHeight,
+			fillStyle: color || 'black',
+			font: `${fontSize}px Arial`,
+			textAlign: 'start',
+			textBaseline: 'top',
+		}));
+
+		if (background) {
+			info.unshift({
+				action: 'fillRect',
+				x: x,
+				y: y,
+				width,
+				height,
+				fillStyle: background,
+			});
+		}
+
+		return info;
+	}
+
+	getHorizontalPositionInScreen(position, width) {
+		switch (position) {
+			case PositionHorizontalEnum.LEFT:
+				return 10;
+			case PositionHorizontalEnum.CENTER:
+				return (this.width - width) / 2;
+			case PositionHorizontalEnum.RIGHT:
+				return this.width - width - 10;
+			default:
+				return 10;
+		}
+	}
+	getVerticalPositionInScreen(position, height) {
+		switch (position) {
+			case PositionVerticalEnum.TOP:
+				return 10;
+			case PositionVerticalEnum.MIDDLE:
+				return (this.height - height) / 2;
+			case PositionVerticalEnum.BOTTOM:
+				return this.height - height - 10;
+			default:
+				return 10;
+		}
+	}
+}
